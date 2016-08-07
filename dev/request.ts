996558cb9cca27836584c28eb8d95674283722a4
dionis <dionisnote@gmail.com>
2016-08-07 18:58:35 +0400

    request class

---
+++ dev/request.ts
@@ -0,0 +1,120 @@
+class Request
+{
+    public xhttp;
+    public response:any;
+    
+    public constructor() 
+    {
+        // get XMLHttpRequest for IE or normal browsers 
+        let xhttp;
+        try {
+            xhttp = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+            xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (E) {
+            xhttp = false;
+            }
+        }
+        if (!xhttp && typeof XMLHttpRequest!='undefined') {
+            xhttp = new XMLHttpRequest();
+        }
+        this.xhttp = xhttp;
+    }
+    
+    /**
+     * GET-Method request
+     */
+    public get(addr, options:Object, callback) 
+    {
+        var self = this;
+        var reqUrl = this.addOptions(addr, options);
+        var timeout = setTimeout( function(){ self.abortRequest() }, 10000 );
+      
+        // open connection  
+        this.xhttp.open('GET', reqUrl, true);
+        // this.xhttp.onreadystatechange = this.checkAnswer();
+        this.xhttp.onreadystatechange = function(){
+            
+            // if request was done
+            if (this.readyState != 4) return;
+
+            // cancel abort timeout
+            clearTimeout(timeout); // clear timeout if was readyState 4
+
+            // if request answer is ok
+            if( this.status == 200 ) {
+                callback(this.response);
+                // console.log(this);
+            } else { // if answer is not Ok
+                console.warn( 'server ansveres with status: ' + this.status );
+            }
+        };
+        
+        this.xhttp.send();
+    }
+    
+    /**
+     *  POST-Method request
+     */
+    public post(addr, options:Object, callback) 
+    {
+        var self = this;
+        var reqUrl = addr;
+        var timeout = setTimeout( function(){ self.abortRequest() }, 10000 );
+        var urlOptions = this.optionsToStringUrl( options );
+      
+        // open connection  
+        this.xhttp.open('POST', reqUrl, true);
+        // this.xhttp.onreadystatechange = this.checkAnswer();
+        this.xhttp.onreadystatechange = function(){
+            
+            // if request was done
+            if (this.readyState != 4) return;
+
+            // cancel abort timeout
+            clearTimeout(timeout); // очистить таймаут при наступлении readyState 4
+
+            // if request answer is ok
+            if( this.status == 200 ) {
+                callback(this.response);
+                // console.log(this);
+            } else { // if answer is not Ok
+                console.warn( 'server ansveres with status: ' + this.status );
+            }
+        };
+        
+        this.xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        this.xhttp.send( urlOptions );
+    
+    }
+    
+    protected addOptions( addr:string, options:Object):string
+    {
+        var reqUrl = addr;
+        // if options set
+        if( Object.keys( options).length ) {
+            reqUrl += '?';
+            reqUrl += this.optionsToStringUrl( options );
+        }
+        return reqUrl;
+    } 
+    
+    protected optionsToStringUrl(options):string
+    {
+        var urlOpt = '';
+        for( var q in options) {
+                urlOpt+= q + '=' + options[q] + '&';
+        }
+        
+        return urlOpt;
+    }
+    
+    protected abortRequest() 
+    {
+        this.xhttp.abort();
+    }
+    
+}
+
+export { Requests };
